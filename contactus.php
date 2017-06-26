d9cc8b8cb36309270107b37d4127f22c04636e29
Mansura Chowdhury <c.mansura@gmail.com>
2017-06-26 17:26:53 -0400

    changed to 2018

--- contactus.php
+++ contactus.php
@@ -32,14 +32,14 @@ mail($my_email,$subject,$message,$headers);
 <meta name="robots" content="noarchive">
 <meta name="description" content="">
 <meta name="keywords" content="environment conference, biofuels conference, desalination conference, wastewater conference, pollution conference, remediation conference, environmental engineering conference, environmental pollution conference, environmental remediation conference, environmental treatment conference, air pollution conference, water pollution conference, wasterwater pollution conference, soil pollution conference, environmental protection, environmental sustainability, environmental development, greenhouse, global warming, climate change, groudwater, oil spills, renewable energy, water treatment, soil treatment, wastewater treatment, environment, biofuels, desalination, wastewater, pollution, remediation, environmental engineering, environmental pollution, environmental remediation, environmental treatment, air pollution, water pollution, wasterwater pollution, soil pollution, environmental protection conference, environmental sustainability conference, environmental development conference, greenhouse conference, global warming conference, climate change conference, groudwater conference, oil spills conference, renewable energy conference, water treatment conference, soil treatment conference, wastewater treatment conference">
-<title>RTESE'17 - Contact Us</title>
+<title>RTESE'18 - Contact Us</title>
 
 <meta name="handheldfriendly" content="true">
 <meta name="mobileoptimized" content="240">
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-<link href="../css/ffhmt.css" rel="stylesheet">
+<link href="../css/style.css" rel="stylesheet">
 <link href='http://fonts.googleapis.com/css?family=Lato:100,300,400,700,900,100italic,300italic,400italic,700italic,900italic|Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
 <link rel="shortcut icon" href="../img/favicon.ico" type="image/x-icon">
 <!--[if IE-9]><html lang="en" class="ie9"><![endif]-->
@@ -61,7 +61,7 @@ mail($my_email,$subject,$message,$headers);
 
 <body>
 <nav id="slide-menu">
-  <h1>RTESE'17</h1>
+  <h1>RTESE'18</h1>
   <ul>
     <li><a href="/">Home</a></li>
     <li><a href="../papers">Paper Submissions</a></li>
@@ -103,8 +103,8 @@ mail($my_email,$subject,$message,$headers);
   </div>
 </div>
         <div class="bg">
-          <h1>International Conference on Recent Trends in<br>Environmental Science and Engineering (RTESE'17)</h1>
-          <p class="subhead">August 23 - 25, 2017 | Toronto, Canada</p>
+          <h1>2<sup>nd</sup> International Conference on Recent Trends in<br>Environmental Science and Engineering (RTESE'18)</h1>
+          <p class="subhead">June 14 - 16, 2018 | Toronto, Canada</p>
 
           <a href="../papers" class="bg-link">Paper Submission</a> <p class="dot">&middot;</p> <a href="../dates" class="bg-link">Important Dates</a> <p class="dot">&middot;</p> <a href="../registration" class="bg-link">Registration</a>
 
@@ -117,9 +117,9 @@ mail($my_email,$subject,$message,$headers);
         <gcse:searchbox-only resultsUrl="../results"></gcse:searchbox-only>
   </div>
 </div>
-</div><br>
-      <p class="body" style="text-align: center!important;">For more information about the RTESE'17 Conference, please view the <b><a href="../RTESE17_CFP.pdf" class="body-link">conference flyer</a></b>.</p>
-
+</div><!-- <br>
+      <p class="body" style="text-align: center!important;">For more information about the RTESE'18 Conference, please view the <b><a href="../RTESE17_CFP.pdf" class="body-link">conference flyer</a></b>.</p> -->
+      <br><br>
         </div>
     </div>
 
@@ -131,8 +131,8 @@ mail($my_email,$subject,$message,$headers);
         </div>
 
         <div class="bg">
-          <h1>International Conference on Recent Trends in<br>Environmental Science and Engineering (RTESE'17)</h1>
-          <p class="subhead">August 23 - 25, 2017 | Toronto, Canada</p>
+          <h1>2<sup>nd</sup> International Conference on Recent Trends in<br>Environmental Science and Engineering (RTESE'18)</h1>
+          <p class="subhead">June 14 - 16, 2018 | Toronto, Canada</p>
 
           <a href="../papers" class="bg-link">Paper Submission</a> <p class="dot">&middot;</p> <a href="../dates" class="bg-link">Important Dates</a> <p class="dot">&middot;</p> <a href="../registration" class="bg-link">Registration</a>
 
@@ -145,10 +145,10 @@ mail($my_email,$subject,$message,$headers);
         <gcse:searchbox-only resultsUrl="../results"></gcse:searchbox-only>
   </div>
 </div>
-</div><br>
+</div><!-- <br>
 
-      <p class="body" style="text-align: center!important; color: #FFF!important;">For more information about the RTESE'17 Conference, please view the <b><a href="../RTESE17_CFP.pdf" class="body-link">conference flyer</a></b>.</p>
-
+      <p class="body" style="text-align: center!important; color: #FFF!important;">For more information about the RTESE'18 Conference, please view the <b><a href="../RTESE17_CFP.pdf" class="body-link">conference flyer</a></b>.</p> -->
+<br><br>
         </div>
         </div> 
       </div>
@@ -162,8 +162,22 @@ mail($my_email,$subject,$message,$headers);
       <div id="main-slider" class="liquid-slider">
     <div>
       <h2 class="title">1</h2>
-      <p class="bold">RTESE 2017:</p>
-      <p class="body">RTESE 2017 will  be held in Toronto, Canada on August 23 - 25, 2017.</p>
+      <p class="bold">RTESE 2018:</p>
+      <p class="body">RTESE 2018 will  be held in Toronto, Canada on June 14 - 16, 2018.</p>
+      
+    <!--   <p class="bold">RTESE'18 Workshop</p>
+      <p class="body">As per popular request, the organizing committee has decided to extend the RTESE'18 conference to three days (now June 14 - 16, 2018). The new high-level schedule is as follows:</p>
+
+      <ul>
+        <li>Day 1: Registration</li>
+        <li>Day 2: Main Track Conference Sessions and Workshop</li>
+        <li>Day 3: Main Track Conference Sessions and Gala dinner or Cruise tour</li>
+      </ul>
+      
+      <p class="body">Registration for the workshop will be 113 CAD (HST included). Workshop attendees will receive a certificate of participation. For registration, please visit: <a href="../registration" class="body-link">here</a>.</p>
+
+      <p class="body">More information to follow; we greatly appreciate your patience!</p> -->
+
     </div>          
     <div>
       <h2 class="title">2</h2>
@@ -173,7 +187,7 @@ mail($my_email,$subject,$message,$headers);
     <div>
       <h2 class="title">3</h2>
       <p class="bold">Become a Sponsor or an Exhibitor</p>
-      <p class="body">RTESE attracts a wide range of researchers in the field of recent trends in environmental science and engineering. As a prominent company in the field of environmental science and engineering, we would like to offer you an exhibit at RTESE. Please visit <a href="../sponsor" class="body-link">Sponsors</a> for more information.</p>
+      <p class="body">RTESE attracts a wide range of researchers in the field of recent trends in environmental science and engineering. As a prominent company in the field of recent trends in environmental science and engineering, we would like to offer you an exhibit at RTESE. Please visit <a href="../sponsor" class="body-link">Sponsors</a> for more information.</p>
     </div>
   </div>
 
@@ -183,7 +197,7 @@ mail($my_email,$subject,$message,$headers);
 <div class="unit unit-s-1 unit-m-1-4-1 unit-l-1-4-1">
   <div class="unit-spacer content">
     <p class="body">We have received your message and we will try our best to get back to you within the next 48 hours.<br><br>
-    Thank you for your interest in RTESE'17.</p>
+    Thank you for your interest in RTESE'18.</p>
   </div>
 </div>
 
@@ -202,13 +216,13 @@ mail($my_email,$subject,$message,$headers);
 <br><br>
 
 
-  <h2>Upcoming Dates</h2>
+ <h2>Upcoming Dates</h2>
 
-<div class="grid events">
+<!-- <div class="grid events">
 <div class="unit unit-s-1 unit-m-1-4 unit-l-1-4">
   <div class="date">
-    <div class="past">May. 26, 2017</div>
-    Jul. 14, 2017
+    <div class="past">May. 26, 2018</div>
+    Jul. 14, 2018
   </div>
 </div>
 
@@ -223,8 +237,8 @@ mail($my_email,$subject,$message,$headers);
 <div class="grid events">
 <div class="unit unit-s-1 unit-m-1-4 unit-l-1-4">
   <div class="date">
-    <div class="past">Jun. 23, 2017</div>
-    Jul. 14, 2017
+    <div class="past">Jun. 23, 2018</div>
+    Jul. 14, 2018
   </div>
 </div>
 
@@ -234,13 +248,13 @@ mail($my_email,$subject,$message,$headers);
      Final Registration Deadline for Authors
   </div>
 </div>
-</div>
+</div> -->
 
 <div class="grid events">
 <div class="unit unit-s-1 unit-m-1-4 unit-l-1-4">
   <div class="date">
-    <!-- <div class="past">May 15, 2017</div> -->
-    Aug. 21 - 23, 2017
+    <!-- <div class="past">May 15, 2018</div> -->
+    Jun. 14 - 16, 2018
   </div>
 </div>
 
@@ -273,55 +287,22 @@ mail($my_email,$subject,$message,$headers);
 
   <div class="unit unit-s-1 unit-m-2-3 unit-l-2-3 contact">
   <div class="unit-spacer">
-  <p class="body">For questions or comments regarding RTESE'17, please fill out the form below:</p>
+  <p class="body">For questions or comments regarding RTESE'18, please fill out the form below:</p>
 
-    <form action="../contactus.php" method="post" enctype="multipart/form-data" name="ContactForm">
-  
-  <table border="0" class="contact">
-    <tbody>
-      <tr>
-        <td class="label">Name:</td>
-        <td class="text"><span id="sprytextfield1">
-              <input name="Name" type="text" id="Name" size="40" autocomplete="off">
-
-              <span class="textfieldRequiredMsg">A value is required.</span></span></td>
-        </tr>
-
-        <tr>
-            <td class="label">Email:</td>
-            <td class="text"><span id="sprytextfield2">
-            <input name="Email" type="text" id="Email" size="40" autocomplete="off">
-            <span class="textfieldRequiredMsg">A value is required.</span><span class="textfieldInvalidFormatMsg">Invalid format.</span></span></td>
-          </tr>
-
-          <tr>
-            <td class="label">Confirm Email:</td>
-             <td class="text"><span id="spryconfirm4">
-              <input name="Confirm Email" type="text" id="Confirm Email" size="40" autocomplete="off">
-              <span class="confirmRequiredMsg">A value is required.</span><span class="confirmInvalidMsg">Emails don't match.</span></span></td>
-          </tr>
-
-          <tr>
-            <td class="label">Subject:</td>
-            <td class="text"><span id="sprytextfield3">
-              <input name="Subject" type="text" id="Subject" size="40" autocomplete="off">
-              <span class="textfieldRequiredMsg">A value is required.</span></span></td>
-          </tr>
-
-          <tr>
-            <td valign="top" class="label">Message:</td>
-            <td class="text"><span id="sprytextarea1">
-              <textarea name="Message" id="Message" cols="31" rows="10" autocomplete="off"></textarea>
-              <span class="textareaRequiredMsg">A value is required.</span></span>
-              <center>
-        <input type="submit" name="Submit" value="Submit" accept="image/jpeg">
-        <input type="reset" name="Reset" value="Reset"></center></td>
-          </tr>
-
-        </tbody></table><br>
-
-        
-</form>
+    <form action="../contactus.php" method="post" enctype="multipart/form-data" name="ContactForm" class="cf">
+  <div class="half left cf">
+    <input style="margin-bottom:0.85em" type="text" name="Name" id="Name" placeholder="Name" required>
+    <input style="margin-bottom:0.85em" type="email" name="Email" id="Email" placeholder="Email address" required>
+    <input type="text" name="Subject" id="Subject" placeholder="Subject" required>
+  </div>
+  <div class="half right cf">
+    <textarea name="Message" type="text" rows="5" name="Message" id="Message" placeholder="Message" required></textarea>
+  </div><br><br>
+  <center class="full right cf"><div class="g-recaptcha" data-sitekey="6LdOFyMTAAAAAPU_lcowyxZIUuivlqaSIuEdK0mz"></div></center>
+  <div class="cf">
+  <center><div class="full right cf"><input type="submit" name="Submit" value="Submit">
+    <input type="reset" name="Reset" value="Reset"></center></div>
+</div></div></form>
     </div>
   </div>
   </div>
@@ -329,7 +310,7 @@ mail($my_email,$subject,$message,$headers);
 
 <div class="copyright">
   <a href="http://international-aset.com">International ASET Inc.</a> | <a href="http://international-aset.com/phplistpublic/?p=subscribe&id=1">Subscribe</a> | <a href="../terms">Terms of Use</a> | <a href="../sitemap">Sitemap</a>
-  <p class="body">&copy; Copyright International ASET Inc., 2016. All rights reserved.</p>
+  <p class="body">© Copyright <script>document.write(new Date().getFullYear())</script>, International ASET Inc. – All Rights Reserved.</p>
   <p class="copyright1">Have any feedback? Please provide them here: <script>var refURL = window.location.protocol + "//" + window.location.host + window.location.pathname; document.write('<a href="http://international-aset.com/feedback/?refURL=' + refURL+'" class="body-link">Feedback</a>');</script></p>
 </div>
 </div>
